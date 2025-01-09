f4cf78f4db0f8c1cbb019202043f750f0710b797
Khalil2155 <khalil.kirkwood@hotmail.co.uk>
2025-01-09 14:48:40 +0000

    temp: disable auth for initial deployment

--- components/providers/auth-provider.tsx
+++ components/providers/auth-provider.tsx
@@ -3,5 +3,8 @@
 import { SessionProvider } from "next-auth/react";
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  // Temporarily return children directly to allow site to build
+  return <>{children}</>;
+  // TODO: Re-enable auth once we set up providers
+  // return <SessionProvider>{children}</SessionProvider>;
 }
