3c7595d35a59534f49588e3723902cf50f6b785d
Khalil2155 <khalil.kirkwood@hotmail.co.uk>
2025-01-10 09:03:24 +0000

    db fixes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,19 +3,31 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  // Create default author
-  const defaultAuthor = await prisma.user.upsert({
-    where: { id: 'default-author' },
+  // Create sample blog post
+  const samplePost = await prisma.post.upsert({
+    where: { id: 'sample-post' },
     update: {},
     create: {
-      id: 'default-author',
-      name: 'Slush Blog',
-      email: 'blog@slush.com',
-      image: '/images/logo.png'
+      id: 'sample-post',
+      title: 'Welcome to Slush Blog',
+      content: `# Welcome to Slush Blog
+
+We're excited to share our journey with you! This blog will be your go-to source for:
+
+- Dating tips and advice
+- Success stories
+- Product updates
+- Community highlights
+
+Stay tuned for more exciting content!`,
+      excerpt: 'Welcome to the official Slush blog where we share dating insights, success stories, and updates.',
+      category: 'Announcements',
+      image: '/images/welcome-post.jpg',
+      author: 'Slush Team'
     },
   });
 
-  console.log({ defaultAuthor });
+  console.log({ samplePost });
 }
 
 main()
